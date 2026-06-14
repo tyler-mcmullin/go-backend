3e872fbd46dde7a5077a04b8267ab2122e207d58
Ecl9pse <tylermcm98@gmail.com>
2026-06-13 19:58:53 -0500

    mongodb integration

--- web_scraper.py
+++ web_scraper.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from pymongo import MongoClient
 import requests
 import json
-import pymongo
+import os
 
 def scrapePost():
     pageURL = "https://www.warhammer-community.com/en-gb/articles/hjdxp86f/saturday-pre-orders-warhammer-40000-armageddon/"
@@ -10,13 +12,30 @@ def scrapePost():
     soup = BeautifulSoup(page.text, "html.parser")
 
     products = soup.find_all("span", attrs = {"class":"font-bold"})
-    date = soup.find("time", attrs = {"class":"copy-bitter-xs whitespace-nowrap hidden @[175px]/video:block @[225px]/article:block xl:!block"})
 
-    print(date.text)
+    fmtProducts = []
     for product in products:
-        print(product.text)
-    
+        fmtProducts.append(product.text)
 
+    date = soup.find("time", attrs = {"class":"copy-bitter-xs whitespace-nowrap hidden @[175px]/video:block @[225px]/article:block xl:!block"}).text.strip()
+
+
+    MONGO_URI = os.getenv("MONGO_URI")
+    client = MongoClient(MONGO_URI)
+    db = client["posts"]
+    collection = db["posts"]
+
+    data = {
+        "products" : fmtProducts,
+        "date" : date,
+        "source" : pageURL,
+    }
+
+    collection.insert_one(data)
+
+
+    
+load_dotenv()
 scrapePost()
 
     
